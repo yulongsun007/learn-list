0d27b0ff92b6349e9f0e6f1d48b94a8e151ccd83
yulongsun007 <a87533887>
2017-08-28 19:42:49 +0800

    1.心跳项目搭建

---
+++ demo-list/demo-all/src/main/java/win/yulongsun/demo/all/net/netty/heartbeat/LoginAuthReqHandler.java
@@ -0,0 +1,9 @@
+package win.yulongsun.demo.all.net.netty.heartbeat;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * @author Sun.Yulong on 2017/8/28.
+ */
+public class LoginAuthReqHandler extends ChannelHandlerAdapter {
+}
