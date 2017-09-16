866d8beabf2e9496df5c610619cccc711b59bf18
yulongsun007 <a87533887>
2017-09-16 10:29:48 +0800

    1、属性覆盖 2、类代码的执行顺序

---
+++ basic-list/java-interview/src/main/java/win/yulongsun/basic/interview/属性覆盖/Person.java
@@ -0,0 +1,33 @@
+package win.yulongsun.basic.interview.属性覆盖;
+
+/**
+ *
+ * 1. 上转型对象 可以替代子类对象去调用子类重写的实例方法。
+ *
+ * 2. 上转型对象 可以调用子类继承的成员变量。
+ *
+ *
+ *
+ * @author Sun.Yulong on 2017/9/14.
+ */
+public class Person {
+    public String name = "person";
+
+    public void say(){
+        System.out.println("person say");
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.name);//-->student
+        //
+        student.say();  //-->student say
+        //
+        //--------------------------------------------------
+        Person person = new Student();
+        System.out.println(person.name); //-->person
+        //
+        person.say(); //-->student say
+//        person.pwd //-->编译报错
+    }
+}
---
+++ basic-list/java-interview/src/main/java/win/yulongsun/basic/interview/类代码的执行顺序/ClassExeSeqCase.java
@@ -0,0 +1,25 @@
+package win.yulongsun.basic.interview.类代码的执行顺序;
+
+/**
+ * Class方法的加载顺序:
+ *
+ * @author Sun.Yulong on 2017/9/14.
+ */
+public class ClassExeSeqCase {
+    {
+        System.out.println("我是构造代码块....");
+    }
+
+    static {
+        System.out.println("我是静态代码块....");
+    }
+
+    public ClassExeSeqCase() {
+        System.out.println("我是构造方法....");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("我是main方法");
+
+    }
+}
