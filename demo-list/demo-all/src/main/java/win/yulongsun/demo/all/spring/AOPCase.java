8130b4cd80387a3e308daa63342726711babd2ae
yulongsun007 <a87533887>
2017-10-11 19:09:48 +0800

    1、添加Spring: AOP

--- demo-list/demo-all/src/main/java/win/yulongsun/demo/all/spring/AOPCase.java
+++ demo-list/demo-all/src/main/java/win/yulongsun/demo/all/spring/AOPCase.java
@@ -1,8 +1,36 @@
 package win.yulongsun.demo.all.spring;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
 /**
- * @author Sun.Yulong on 2017/9/30.
+ * @author Sun.YuLong on 2017/9/30.
+ *
+ * http://blog.csdn.net/xiaoxian8023/article/details/17258933
+ *
  */
-//@Aspect
+@Aspect
 public class AOPCase {
+
+    @Pointcut("execution(* find*(..))")
+    private void aspectjMethod() {
+    }
+
+    @Before("aspectjMethod()")
+    public void beforeAdvice(JoinPoint joinPoint) {
+        System.out.println("beforeAdvice");
+    }
+
+    @After("aspectjMethod()")
+    public void afterAdvice(JoinPoint joinPoint) {
+        System.out.println("afterAdvice");
+    }
+
+    @Around("aspectjMethod()")
+    public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object obj = proceedingJoinPoint.proceed();
+        return obj;
+    }
+
 }
