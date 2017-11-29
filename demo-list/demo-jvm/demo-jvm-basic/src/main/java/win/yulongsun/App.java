c61c8aa9514a8d2b7a130ec4447d06ba8ac9af48
yulongsun007 <a87533887>
2017-11-29 15:58:27 +0800

    添加demo-jvm

---
+++ demo-list/demo-jvm/demo-jvm-basic/src/main/java/win/yulongsun/App.java
@@ -0,0 +1,13 @@
+package win.yulongsun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
