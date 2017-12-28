46eed4b1f4c69865852360ad77dad4cfbca34be9
yulongsun007 <a87533887>
2017-12-28 21:33:17 +0800

    添加jvm 打对象进入老年代 case

---
+++ demo-list/demo-jvm/demo-jvm-basic/src/main/java/win/yulongsun/demo/jvm/basic/page4/LargeObjectTestCase.java
@@ -0,0 +1,20 @@
+package win.yulongsun.demo.jvm.basic.page4;
+
+/**大对象直接进入老年代
+ * 前提：新生代使用ParNewGC. 使用PS无效
+ * @author Sun.YuLong on 2017/12/22.
+ */
+public class LargeObjectTestCase {
+
+    /**
+     * -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseAdaptiveSizePolicy
+     *  -XX:PretenureSizeThreshold=3145728
+     *  -XX:+UseParNewGC
+     *  //
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        byte[] bytes = new byte[5 * 1024 * 1024];
+    }
+}
