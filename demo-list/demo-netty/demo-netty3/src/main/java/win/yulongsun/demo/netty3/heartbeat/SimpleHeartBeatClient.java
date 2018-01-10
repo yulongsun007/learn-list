c4ca7c596e5913425f48412829401e9e1f6acb84
yulongsun007 <a87533887>
2018-01-10 20:10:12 +0800

    修改netty3目录

--- demo-list/demo-netty/demo-netty3/src/main/java/win/yulongsun/demo/netty3/heartbeat/SimpleHeartBeatClient.java
+++
@@ -1,7 +0,0 @@
-package win.yulongsun.demo.netty3.heartbeat;
-
-/**
- * @author Sun.YuLong on 2017/11/29.
- */
-public class SimpleHeartBeatClient {
-}
---
+++ demo-list/demo-netty/demo-netty3/src/main/java/win/yulongsun/demo/netty3/heartbeat/client/Netty3HeartBeatClient.java
@@ -0,0 +1,34 @@
+package win.yulongsun.demo.netty3.heartbeat.client;
+
+import org.jboss.netty.bootstrap.ClientBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
+import win.yulongsun.demo.netty3.heartbeat.client.handler.Netty3HeartBeatClientHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Sun.YuLong on 2017/11/29.
+ */
+public class Netty3HeartBeatClient {
+
+    public static void main(String[] args) {
+        ClientBootstrap clientBootstrap = new ClientBootstrap();
+        clientBootstrap.setFactory(new NioClientSocketChannelFactory());
+        clientBootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            @Override
+            public ChannelPipeline getPipeline() throws Exception {
+                ChannelPipeline pipeline = Channels.pipeline();
+                pipeline.addLast("stringDecoder", new StringDecoder());
+                pipeline.addLast("stringEncoder", new StringEncoder());
+                pipeline.addLast("heartBeatClientHandler", new Netty3HeartBeatClientHandler());
+                return pipeline;
+            }
+        });
+        clientBootstrap.connect(new InetSocketAddress(8888));
+    }
+}
