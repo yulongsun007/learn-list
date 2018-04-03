b25abc80a9a916663670d45b5c6780868c4f61d1
yulongsun007 <a87533887>
2018-04-03 20:29:31 +0800

    添加fastdfs

---
+++ demo-list/demo-springboot/demo-springboot-fastdfs/src/main/java/win/yulongsun/demo/springboot/fastdfs/DemoFastdfsApp.java
@@ -0,0 +1,10 @@
+package win.yulongsun.demo.springboot.fastdfs;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sun.YuLong on 2018/4/3.
+ */
+@SpringBootApplication
+public class DemoFastdfsApp {
+}
