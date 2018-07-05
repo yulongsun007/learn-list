cd86c219606b962d5dc57ceb0fb83442fa17a4cf
yulongsun007 <a87533887>
2018-07-06 01:19:20 +0800

    添加demo-springboot-shiro

---
+++ demo-list/demo-springboot/demo-springboot-shiro/src/main/java/win/yulongsun/demo/springboot/shiro/DemoSpringbootShiroApplication.java
@@ -0,0 +1,12 @@
+package win.yulongsun.demo.springboot.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootShiroApplication.class, args);
+    }
+}
---
+++ demo-list/demo-springboot/demo-springboot-shiro/src/test/java/win/yulongsun/demo/springboot/shiro/DemoSpringbootShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package win.yulongsun.demo.springboot.shiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringbootShiroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
