f569a7c2a6e17afe33ce05d07ddf2c547453a64f
yulongsun007 <a87533887>
2018-07-22 16:13:09 +0800

    整理demo-springcloud工程

---
+++ demo-list/demo-springcloud/demo-springcloud-zuul/src/main/java/win/yulongsun/demo/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package win.yulongsun.demo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Sun.YuLong on 2018/7/22.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
--- demo-list/demo-springcloud/microservice-simple-provider-user/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO  user (id,username,name,age,balance) VALUES (
-1,'account1','zhangsan',20,100.00
-),(
-2,'account2','lisi',28,180.00
-),(
-3,'account3','wangwu',32,280.00
-);
